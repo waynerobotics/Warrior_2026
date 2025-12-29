e7a9005ce6d07181ebef87bf023668dd3e4ebda2
alimyust <alimyust@gmail.com>
2025-12-29 00:24:32 -0500

    Added joy launch file to work with turtlebot3

--- warrior_joy/setup.py
+++ warrior_joy/setup.py
@@ -12,6 +12,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/joy_2stick.launch.py']),
         ('share/' + package_name + '/launch', ['launch/joy_swerve.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/joy_turtle.launch.py']),
+        ('share/' + package_name + '/config', ['config/joystick.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
