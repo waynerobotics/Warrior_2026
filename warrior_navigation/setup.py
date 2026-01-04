d4a8f7157b2552956fcbe128073a1ab6b0398919
alimyust <alimyust@gmail.com>
2026-01-03 22:49:00 -0500

    add nav2/slamtoolbox launch files

---
+++ warrior_navigation/setup.py
@@ -0,0 +1,33 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'warrior_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/maps', glob('maps/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alimyust',
+    maintainer_email='alimyust@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
