1a43277c6cc3519de5a34920aaf571d58d9276f6
Sam Redman <cp1590@wayne.edu>
2026-02-13 19:05:50 -0500

    online_async_launch.py now has correct location of mapper config file

--- warrior_localization/launch/online_async_launch.py
+++ warrior_localization/launch/online_async_launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
         description='Use simulation/Gazebo clock')
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("warrior_navigation"),
+        default_value=os.path.join(get_package_share_directory("warrior_localization"),
                                    'config', 'mapper_params_online_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
