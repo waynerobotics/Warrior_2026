f965135c4bc8f2b48caf52c4896336909885ff1c
alimyust <alimyust@gmail.com>
2026-05-31 13:08:58 -0400

    fixed sim_time TF error

--- warrior_navigation/launch/nav.launch.py
+++ warrior_navigation/launch/nav.launch.py
@@ -57,6 +57,7 @@ def launch_setup(context, *args, **kwargs):
         name='nav2_gps_waypoint_follower',
         output='screen',
         parameters=[{
+            'use_sim_time': use_sim.perform(context),
             'waypoint_file': selected_waypoint_file,
             'action_name': 'navigate_to_pose',
         }],
